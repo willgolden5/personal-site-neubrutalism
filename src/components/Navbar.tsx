0bdbccdf6bafcea702691270de2e8df55af7a941
William Golden <wtgolden5@gmail.com>
2024-02-08 14:09:26 -0500

    cursor pointer nav items

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -32,7 +32,7 @@ const Navbar = ({ projectsRef, homeRef }: NavbarProps) => {
     <nav className="fixed left-0 top-0 z-10 mx-auto flex h-20 w-full items-center border-b-4 border-black bg-white px-5 m500:h-16">
       <div className="mx-auto flex w-[1300px] max-w-full items-center justify-between">
         <a
-          className="text-3xl font-bold m500:text-xl capitalize"
+          className="text-3xl font-bold m500:text-xl capitalize cursor-pointer"
           onClick={handleHomeClick}
         >
           GLDN
@@ -47,13 +47,13 @@ const Navbar = ({ projectsRef, homeRef }: NavbarProps) => {
           </a> */}
           <p
             onClick={handleHomeClick}
-            className="flex items-center justify-center font-bold text-2xl m500:text-xl"
+            className="flex items-center justify-center font-bold text-2xl m500:text-xl cursor-pointer"
           >
             Home
           </p>
           <p
             onClick={handleProjectsClick}
-            className="flex items-center justify-center font-bold text-2xl m500:text-xl"
+            className="flex items-center justify-center font-bold text-2xl m500:text-xl cursor-pointer"
           >
             Projects
           </p>
